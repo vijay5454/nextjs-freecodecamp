6cf613931e26ee5522aa04bf5d111d70d513972b
vijay5454 <vijaythalaiva5454@gmail.com>
2024-12-22 17:29:20 +0530

    disable eslint for particular line.

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const UserProfile = ({ params }: any) => {
   return <div>UserProfile {params.id}</div>;
 };
